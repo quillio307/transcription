f0229f2254a69508bd22a34ba0b3c18570038911
Anoop Jain <anoopj08@gmail.com>
2017-10-04 00:56:15 -0400

    fuck this shit

--- src/transcript.py
+++ src/transcript.py
@@ -1,3 +1,5 @@
+import io
+import pdb
 def transcribe_gcs(gcs_uri):
     """Asynchronously transcribes the audio file specified by the gcs_uri."""
     from google.cloud import speech
@@ -21,14 +23,52 @@ def transcribe_gcs(gcs_uri):
         print('Transcript: {}'.format(result.alternatives[0].transcript))
         print('Confidence: {}'.format(result.alternatives[0].confidence))
 
+# [START def_transcribe_file]
+def transcribe_file(speech_file):
+    from google.cloud import speech
+    from google.cloud.speech import enums
+    from google.cloud.speech import types
+    print("starting")
+    client = speech.SpeechClient()
+
+    # [START migration_async_request]
+    with io.open(speech_file, 'rb') as audio_file:
+        content = audio_file.read()
+
+    audio = types.RecognitionAudio(content=content)
+    config = types.RecognitionConfig(
+        encoding=enums.RecognitionConfig.AudioEncoding.FLAC,
+        sample_rate_hertz=44100,
+        language_code='en-US',
+        max_alternatives=16)
+
+    # [START migration_async_response]
+    operation = client.long_running_recognize(config, audio)
+    # [END migration_async_request]
+
+    print('Waiting for operation to complete...')
+    response = operation.result(timeout=90)
+    print(operation.metadata())
+    # Print the first alternative of all the consecutive results.
+    i = 0;
+    for result in response.results:
+        print('Transcript: {}'.format(result.alternatives[0].transcript))
+        print('Confidence: {}'.format(result.alternatives[0].confidence))
+        for word in result.alternatives[0].words:
+            print(word)
+    # [END migration_async_response]
+# [END def_transcribe_file]
+
 def main():
+
     from googleapiclient.discovery import build
     from oauth2client.client import GoogleCredentials
     credentials = GoogleCredentials.get_application_default()
     service = build('compute', 'v1', credentials=credentials)
-    transcribe_gcs("gs://cloud-samples-tests/speech/brooklyn.flac")
 
 
+    #transcribe_gcs("gs://cloud-samples-tests/speech/brooklyn.flac")
+    transcribe_file("../test_audio_files/test_audio_2_mono.flac");
 
 if __name__ == "__main__":
     main()
