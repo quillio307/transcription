a7bb01a9f649d1e303bbda098538a1081d5af3c5
Ammar Husain <ammo700@gmail.com>
2017-10-30 10:58:34 -0400

    Connecting to GST properly

--- app.py
+++ app.py
@@ -2,12 +2,27 @@ from flask import Flask
 from flask_sockets import Sockets
 import random
 
+from google.cloud import speech
+from google.cloud.speech import enums
+from google.cloud.speech import types
+
 app = Flask(__name__)
 sockets = Sockets(app)
+
+# Audio recording parameters
+RATE = 16000
+CHUNK = int(RATE / 10)  # 100ms
+TIME = 0.00
+
+a = 5
 
 
 @sockets.route('/echo')
 def echo_socket(ws):
+	language_code = 'en-US'  # a BCP-47 language tag
+	client = speech.SpeechClient()
+	config = types.RecognitionConfig( encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16, sample_rate_hertz=RATE, language_code=language_code, enable_word_time_offsets=1)
+	streaming_config = types.StreamingRecognitionConfig( config=config, interim_results=False)
 	rand = random.randrange(100)
 	while not ws.closed:
 		message = ws.receive()
