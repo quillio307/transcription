252d704a49aff7066f58f40c7fc740a2d5f6f86b
Ammar Husain <ammo700@gmail.com>
2017-10-31 12:55:19 -0400

    Added node version of GST

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sockets import Sockets
 import random
+import json
 
 from google.cloud import speech
 from google.cloud.speech import enums
@@ -16,18 +17,41 @@ TIME = 0.00
 
 a = 5
 
+class SpeechToTextWS(object):
+
+	def __init__(self, ws):
+		self._ws = ws
+		self._client = speech.SpeechClient()
+		self._config = types.RecognitionConfig( encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16, sample_rate_hertz=RATE, language_code='en-US', enable_word_time_offsets=1)
+		self._sconfig = types.StreamingRecognitionConfig( config=self._config, interim_results=False)
+
+	def generator(self):
+		while not self._ws.closed:
+			chunk = str.encode(self._ws.receive().encode('ascii','replace'))
+			data = [chunk]
+			yield b''.join(data)
+
+	def recog_loop(self):
+		generator = self.generator()
+		res = self._client.streaming_recognize(self._sconfig, (types.StreamingRecognizeRequest(audio_content=content) for content in generator))
+		for response in res:
+			if not response.results:
+				continue
+
+			result = response.results[0]
+			if not result.alternatives:
+				continue
+
+			transcript = result.alternatives[0].transcript
+
+			ws.send(transcript)
+
 
 @sockets.route('/echo')
 def echo_socket(ws):
-	language_code = 'en-US'  # a BCP-47 language tag
-	client = speech.SpeechClient()
-	config = types.RecognitionConfig( encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16, sample_rate_hertz=RATE, language_code=language_code, enable_word_time_offsets=1)
-	streaming_config = types.StreamingRecognitionConfig( config=config, interim_results=False)
-	rand = random.randrange(100)
-	while not ws.closed:
-		message = ws.receive()
-		print(message)
-		ws.send("This is my random number: " + str(rand))
+	stt = SpeechToTextWS(ws)
+	stt.recog_loop()
+		
 
 
 @app.route('/')
