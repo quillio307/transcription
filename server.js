42d838c2553394af4bc93eb754966606350abbcc
Ammar Husain <ammo700@gmail.com>
2017-11-01 16:26:11 -0400

    Added node env, clustering ws process for max concurrency

--- server.js
+++ server.js
@@ -1,79 +1,97 @@
-const WebSocket = require('ws');
-const url = require('url');
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
 
-// Imports the Google Cloud client library
-const Speech = require('@google-cloud/speech');
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
 
-// Instantiates a client
-const speech = Speech();
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
 
-// The encoding of the audio file, e.g. 'LINEAR16'
-const encoding = 'LINEAR16';
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+} else {
+	const WebSocket = require('ws');
+	const url = require('url');
 
-// The sample rate of the audio file in hertz, e.g. 16000
+	// Imports the Google Cloud client library
+	const Speech = require('@google-cloud/speech');
 
+	// Instantiates a client
+	const speech = Speech();
 
-// The BCP-47 language code to use, e.g. 'en-US'
-const languageCode = 'en-US';
+	// The encoding of the audio file, e.g. 'LINEAR16'
+	const encoding = 'LINEAR16';
 
-
-
-
-const wss = new WebSocket.Server({
-	port: process.env.PORT
-});
-
-wss.on('connection', function connection(ws, req) {
-	// Create a recognize stream
-	let rt = url.parse(req.url, true).query.sampRate;
-	if(rt === undefined){
-		process.stdout.write(`Denied connection to ${req.url}\n`);
-		ws.close();
-		return;
-	}
-	process.stdout.write(`Accepted connection to ${req.url}\n`);
-	const sampleRateHertz = parseInt(rt);
-
-	const request = {
-		config: {
-			encoding: encoding,
-			sampleRateHertz: sampleRateHertz,
-			languageCode: languageCode
-		},
-		interimResults: true // If you want interim results, set this to true
-	};
-
-	let recognizeStream;
-	initStream();
+	// The sample rate of the audio file in hertz, e.g. 16000
 
 
-	ws.on('message', function incoming(message) {
-		recognizeStream.write(message);
+	// The BCP-47 language code to use, e.g. 'en-US'
+	const languageCode = 'en-US';
+
+
+
+
+	const wss = new WebSocket.Server({
+		port: process.env.PORT
 	});
 
-	ws.on('close', function (code, reason) {
-		process.stdout.write("Client connection closed\n");
+	wss.on('connection', function connection(ws, req) {
+		// Create a recognize stream
+		let rt = url.parse(req.url, true).query.sampRate;
+		if(rt === undefined){
+			process.stdout.write(`Denied connection to ${req.url}\n`);
+			ws.close();
+			return;
+		}
+		process.stdout.write(`Accepted connection to ${req.url}\n`);
+		const sampleRateHertz = parseInt(rt);
+
+		const request = {
+			config: {
+				encoding: encoding,
+				sampleRateHertz: sampleRateHertz,
+				languageCode: languageCode
+			},
+			interimResults: true // If you want interim results, set this to true
+		};
+
+		let recognizeStream;
+		initStream();
+
+
+		ws.on('message', function incoming(message) {
+			recognizeStream.write(message);
+		});
+
+		ws.on('close', function (code, reason) {
+			process.stdout.write("Client connection closed\n");
+		});
+
+		ws.on('error', function (e) {
+			process.stdout.write(`Socket error:\n${e}\n`);
+			ws.terminate();
+		});
+
+		function initStream() {
+			recognizeStream = speech.streamingRecognize(request)
+				.on('error', (err) => {
+					console.log(err);
+					console.log("Starting new stream\n")
+					initStream();
+				})
+				.on('data', (data) => {
+					try{
+						ws.send(JSON.stringify(data.results));	
+					}catch(e){
+						process.stdout.write("Client send error\n");
+						if(ws.isAlive === false) ws.terminate();
+					}
+				});
+		}
 	});
 
-	ws.on('error', function (e) {
-		process.stdout.write(`Socket error:\n${e}\n`);
-		ws.terminate();
-	});
-
-	function initStream() {
-		recognizeStream = speech.streamingRecognize(request)
-			.on('error', (err) => {
-				console.log(err);
-				console.log("Starting new stream\n")
-				initStream();
-			})
-			.on('data', (data) => {
-				try{
-					ws.send(JSON.stringify(data.results));	
-				}catch(e){
-					process.stdout.write("Client send error\n");
-					if(ws.isAlive === false) ws.terminate();
-				}
-			});
-	}
-});
+  console.log(`Worker ${process.pid} started`);
+}
